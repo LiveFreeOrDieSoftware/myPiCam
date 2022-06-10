402cc806a1e198fe4d4eb4054bd4beae5f684798
mlambi <mlambi@users.noreply.github.com>
2022-06-10 13:41:01 -0400

    Added fan-control script

---
+++ fan-control.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# sample fan control script snatched off the interweb
+# the thing to note here is:
+# - gpiozero
+# - OutputDevice(pin_number)
+# - it is on or off, no speed control: PWM could solve that
+# - additional work to get it to start at boot
+
+import time
+
+from gpiozero import OutputDevice
+
+
+ON_THRESHOLD = 65  # (degrees Celsius) Fan kicks on at this temperature.
+OFF_THRESHOLD = 55  # (degress Celsius) Fan shuts off at this temperature.
+SLEEP_INTERVAL = 5  # (seconds) How often we check the core temperature.
+GPIO_PIN = 4  # Which GPIO pin you're using to control the fan.
+
+
+def get_temp():
+    """Get the core temperature.
+
+    Read file from /sys to get CPU temp in temp in C *1000
+
+    Returns:
+        int: The core temperature in thousanths of degrees Celsius.
+    """
+    with open('/sys/class/thermal/thermal_zone0/temp') as f:
+        temp_str = f.read()
+
+    try:
+        return int(temp_str) / 1000
+    except (IndexError, ValueError,) as e:
+        raise RuntimeError('Could not parse temperature output.') from e
+
+if __name__ == '__main__':
+    # Validate the on and off thresholds
+    if OFF_THRESHOLD >= ON_THRESHOLD:
+        raise RuntimeError('OFF_THRESHOLD must be less than ON_THRESHOLD')
+
+    fan = OutputDevice(GPIO_PIN)
+
+    while True:
+        temp = get_temp()
+
+        # Start the fan if the temperature has reached the limit and the fan
+        # isn't already running.
+        # NOTE: `fan.value` returns 1 for "on" and 0 for "off"
+        if temp > ON_THRESHOLD and not fan.value:
+            fan.on()
+
+        # Stop the fan if the fan is running and the temperature has dropped
+        # to 10 degrees below the limit.
+        elif fan.value and temp < OFF_THRESHOLD:
+            fan.off()
+
+        time.sleep(SLEEP_INTERVAL)
